238a4bf5b46415f0a8c23838c94a71890a0c4858
Aliunay <128824019+Aliunay@users.noreply.github.com>
2024-08-01 23:53:11 +0300

    Podkes
    Flutter ile geliştirilmiştir.

---
+++ podkes_demo/podkes_demo/lib/feature/view/now_play_page/now_play_page.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+
+class NowPlay extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  NowPlay({required this.title, required this.subtitle, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final isLandscape = size.width > size.height;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF0E0D18), // Arka plan rengini
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context); // Geri gitmek için Navigator.pop kullanabilirsiniz
+          },
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+        ),
+        backgroundColor: const Color(0xFF0E0D18),
+        title: Text(
+          'Now Playing',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Hero(
+              tag: image,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                  height: isLandscape ? size.height * 0.4 : size.height * 0.3,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              subtitle,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'assets/images/muzik.png', // Dalga formu png buraya ekledik
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '24:32',
+                  style: TextStyle(color: Colors.white54),
+                ),
+                Text(
+                  '34:00',
+                  style: TextStyle(color: Colors.white54),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.skip_previous, color: Colors.white),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 16.0),
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.play_arrow,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(width: 16.0),
+                IconButton(
+                  icon: const Icon(Icons.skip_next, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
